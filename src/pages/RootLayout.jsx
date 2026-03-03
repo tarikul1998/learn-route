6f709a765d263ee6e3aca393840ee89540cf0cfb
Md Tarikul Islam <tarikulnayan1998@gmail.com>
2026-03-03 19:35:10 +0600

    added files

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -18,10 +18,10 @@ export default RootLayout;
 
 
 // RootLayout er ki aage theke jana sombhob User kon path e ase ?
-// Mane ei Layout (Nav) sharing badeo, Kokhon take Home/about dekhaite hobe, inside this component
-// Tai React special component Outlet use korbo
-// Outlet is a placeholder component (It's just a parameter)
+// Mane ei Layout (Nav) sharing badeo, Kokhon take Home/about dekhaite hobe, Inside this component
+// Tai React er special component Outlet use korbo
 
+// Outlet is a placeholder component (parameter)
 
 // function add(a, b) {
 //     return a + b;
